a540d2b28db4fb6e3c851887cc866fe21d68fca6
nkorchizhinskiy <korchizhinskiy@gmail.com>
2022-07-22 01:40:30 +0700

    Use function by add user into database.

--- tgbot/routes/user/welcome.py
+++ tgbot/routes/user/welcome.py
@@ -7,6 +7,8 @@ from tgbot.filters.role import Role_Filter
 from tgbot.models.role import UserRole
 from tgbot.states.admin.menu import AdminStartMenu 
 
+from tgbot.services.repository import Repo
+
 
 
 user_welcome_router = Router()
@@ -15,7 +17,8 @@ user_welcome_router = Router()
 # @admin_welcome_router.message(Role_Filter(user_role=[UserRole.ADMIN, UserRole.USER]), commands=["check"])
 
 @user_welcome_router.message(Role_Filter(user_role=UserRole.USER), Command(commands=["start"]))
-async def admin_welcome(message: Message, state: FSMContext) -> None:
+async def admin_welcome(message: Message, repo: Repo, state: FSMContext) -> None:
+    await repo.add_user(message.from_user.id, message.from_user.first_name)
     await message.reply(
             f"Привет, {message.from_user.first_name}"
             )
