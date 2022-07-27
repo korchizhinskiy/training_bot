5335a1e34a5ececb534d1d01c92af26cd9cd1ea7
nkorchizhinskiy <korchizhinskiy@gmail.com>
2022-07-27 16:26:51 +0700

    Add callback query and editing markup.

--- tgbot/routes/admin/admin_menu.py
+++ tgbot/routes/admin/admin_menu.py
@@ -2,7 +2,9 @@ from aiogram.dispatcher.filters import Command
 from aiogram.dispatcher.fsm.context import FSMContext
 from aiogram.dispatcher.router import Router
 
-from aiogram.types import Message
+from aiogram.types import CallbackQuery, Message
+from tgbot.filters.role import Role_Filter
+from tgbot.models.role import UserRole
 
 from tgbot.keyboards.inline_keyboards.admin.menu import Button, get_menu_markup
 from tgbot.states.admin.menu import AdminStartMenu
@@ -12,10 +14,22 @@ from tgbot.states.admin.menu import AdminStartMenu
 admin_menu_router = Router()
 
 
-@admin_menu_router.message(Command(commands=["admin"]))
+@admin_menu_router.message(Role_Filter(user_role=UserRole.ADMIN), Command(commands=["admin"]))
 async def admin_welcome(message: Message, state: FSMContext) -> None:
+    """Welcome admin in chat and set menu's state."""
     markup = get_menu_markup([
-        [Button(text="Привет", callback_data="Приветствие"), Button(text="Пока", callback_data="Прощание")]
+        [Button(text="Упражнения", callback_data="exercises")], 
+        [Button(text="Настройки", callback_data="settings")]
         ])
-    await message.reply("Привет, Администратор! Вы успешно прошли авторизацию.", reply_markup=markup)
+    await message.answer("Привет, Администратор! Вы успешно прошли авторизацию.", reply_markup=markup)
     await state.set_state(AdminStartMenu.admin_menu)
+
+
+@admin_menu_router.callback_query(Role_Filter(user_role=UserRole.ADMIN), text="exercises", state=AdminStartMenu.admin_menu)
+async def admin_exercises(call: CallbackQuery, state: FSMContext) -> None:
+    """Display exercise's menu in chat"""
+    new_markup = get_menu_markup([
+        [Button(text="Добавить", callback_data="add_exercise"), Button(text="Изменить", callback_data="change_exercise")],
+        [Button(text="Удалить", callback_data="delete_exercise"), Button(text="Просмотреть все", callback_data="print_exercise")]
+        ])
+    await call.message.edit_text(text="Пункт управления упражнениями", reply_markup=new_markup)
