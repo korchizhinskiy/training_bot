1596e43d15137de7790889276d6f9a30607a5976
nkorchizhinskiy <korchizhinskiy@gmail.com>
2022-07-27 16:30:39 +0700

    Add states to Admin menu (Add, change)

--- tgbot/states/admin/menu.py
+++ tgbot/states/admin/menu.py
@@ -6,3 +6,25 @@ class AdminStartMenu(StatesGroup):
     """Start menu."""
     admin_menu = State()
 
+
+
+
+class AdminExerciseMenu(StatesGroup):
+
+    class AddExercise(StatesGroup):
+        """Add exercise Group."""
+        input_name = State()
+        input_description = State()
+
+
+    class ChangeExercise(StatesGroup):
+        """Change exercise info Group."""
+        parameter_selection = State()
+        input_name = State()
+        input_description = State()
+
+
+    """Exercise menu."""
+    add_exercise = AddExercise()
+    change_exercise = ChangeExercise()
+
