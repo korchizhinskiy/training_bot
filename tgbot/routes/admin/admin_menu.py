e011dfd84799c8a13ac35f4b0e1b5710b70ff5f9
korchizhinskiy <korchizhinskiy@gmail.com>
2022-08-04 19:45:33 +0300

    Add removal exercise from list(database)

--- tgbot/routes/admin/admin_menu.py
+++ tgbot/routes/admin/admin_menu.py
@@ -10,7 +10,7 @@ from tgbot.models.role import UserRole
 from tgbot.keyboards.inline_keyboards.admin.menu import Button, get_menu_markup
 from tgbot.states.admin.menu import AdminExerciseMenu
 
-from tgbot.services.repository import AdminRepo, Repo
+from tgbot.services.repository import AdminRepo
 
 
 admin_menu_router = Router()
@@ -80,7 +80,7 @@ async def admin_input_discription(message: Message, state: FSMContext):
     await state.update_data(exercise_description=message.text)
     exercise_discription = message.text.strip()
     markup = await get_menu_markup([
-        [Button(text="Подтвердить", callback_data="output_result")],
+        [Button(text="Подтвердить", callback_data="add_output_result")],
         [Button(text="Изменить описание", callback_data="input_description"), Button(text="Удалить", callback_data="menu")]
         ])
     await message.answer(
@@ -91,17 +91,57 @@ async def admin_input_discription(message: Message, state: FSMContext):
            )
     await state.set_state(None)
 
-@admin_menu_router.callback_query(Role_Filter(user_role=UserRole.ADMIN), text="output_result", flags={"database_type": "admin_repo"})
-async def print_result(call: CallbackQuery, state: FSMContext, repo: AdminRepo):
+@admin_menu_router.callback_query(Role_Filter(user_role=UserRole.ADMIN), text="add_output_result", flags={"database_type": "admin_repo"})
+async def print_result_of_add(call: CallbackQuery, state: FSMContext, repo: AdminRepo):
     """Print result of exercise and add to database."""
     markup = await get_menu_markup([
         [Button(text="Добавить упражнение ещё", callback_data="add_exercise"), Button(text="Вернуться в меню", callback_data="menu")]
         ])
     await call.message.edit_text("Вы успешно добавили упражнение.", reply_markup=markup)
     state_data = await state.get_data()
-    print(state_data["exercise_name"])
     await repo.add_exercise(exercise_name=state_data['exercise_name'], exercise_description=state_data['exercise_description'])
     
+#!<--- Delete Exercise --->
+@admin_menu_router.callback_query(Role_Filter(user_role=UserRole.ADMIN), text="delete_exercise")
+async def ask_exercise_name(call: CallbackQuery, state: FSMContext):
+    """ Ask name of exercise for delete. """
+    await call.message.edit_text("Введите название упражнения")
+    await state.set_state(AdminExerciseMenu.delete_exercise.read_name)
+
+
+@admin_menu_router.message(Role_Filter(user_role=UserRole.ADMIN), state=AdminExerciseMenu.delete_exercise.read_name)
+async def get_confirmation(message: Message, state: FSMContext):
+    """ Read input name of exercise. """
+    exercise_name = message.text.strip().capitalize()
+    markup = await get_menu_markup([
+        [Button(text="Подтвердить", callback_data="delete_output_result"), Button(text="Отменить", callback_data="menu")]
+        ])
+    await message.answer(f"Вы уверены, что хотите удалить упражнение:\n{exercise_name}", reply_markup=markup)
+    await state.update_data(exercise_name=exercise_name)
+    await state.set_state(None)
+
+
+@admin_menu_router.callback_query(Role_Filter(user_role=UserRole.ADMIN), text="delete_output_result", flags={"database_type": "admin_repo"})
+async def print_result_of_delete(call: CallbackQuery, repo: AdminRepo, state: FSMContext):
+    """ Delete exercise from database. """
+    state_data = await state.get_data()
+    result = await repo.delete_exercise(exercise_name=state_data['exercise_name'])
+    if result == "DELETE 1":
+        markup = await get_menu_markup([
+            [Button(text="Удалить ещё", callback_data="delete_exercise"), Button(text="Вернуться в меню", callback_data="menu")]
+            ])
+        await call.message.edit_text(f"Упражнение {state_data['exercise_name']} успешно удалено.", reply_markup=markup)
+    else:
+        await call.message.edit_text("<b>Упражнение не было удалено, так как его не существует</b>")
+
+
+#!<--- Print Exercise --->
+@admin_menu_router.callback_query(Role_Filter(user_role=UserRole.ADMIN), text="print_exercise", flags={"database_type": "admin_repo"})
+async def ask_exercise_name(call: CallbackQuery, repo: AdminRepo):
+    """ Print all exercise names"""
+    exercises = await repo.print_exercises()
+    await call.message.edit_text("<b>Список упражнений:</b>\n\n" + "\n".join(exercises))
+
 
 @admin_menu_router.message(Role_Filter(user_role=UserRole.ADMIN))
 async def delete_false_message(message: Message):
