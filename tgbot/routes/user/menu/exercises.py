60badf6f2d73d21befa9993e926700b2be3684c5
korchizhinskiy <korchizhinskiy@gmail.com>
2022-08-14 18:30:22 +0300

    Reduce for function pagination

--- tgbot/routes/user/menu/exercises.py
+++ tgbot/routes/user/menu/exercises.py
@@ -11,7 +11,8 @@ from tgbot.models.role import UserRole
 from tgbot.services.repository import UserRepo
 from tgbot.states.user.menu import UserTrainingMenu
 
-from tgbot.misc.callback_data import ExerciseInformationCallbackData, ExercisePaginationCallbackData
+from tgbot.misc.callback_data import ExercisePaginationCallbackData
+from tgbot.misc.pagination import get_pagination
 
 
 
@@ -23,68 +24,17 @@ logging.basicConfig(
         )
 
 #!<--- EXERCISE --->
+
 @exercise_router.message(Role_Filter(user_role=UserRole.USER), Command(commands=["exercises"]), flags={"database_type": "user_repo"})
 async def print_exercises(message: Message, repo: UserRepo, state: FSMContext) -> None:
-    exercises: tuple = await repo.get_exercises()
-
-    logger.info(f"Количество - {len(exercises)}")
-    if (len(exercises)) % 5 == 0:
-        max_page = len(exercises)//5 if len(exercises)//5 != 0 else 1
-    else:
-        max_page = (len(exercises)//5) + 1
-
-    exercise_markup = [[Button(text=exercise, callback_data=exercise)] for exercise in exercises[0:5]]
-    navigation_markup = [
-             Button(text=f"{1}/{max_page}", callback_data=1),
-             Button(text=f"{2} >>", callback_data=ExercisePaginationCallbackData(page_number=2).pack())
-             ]
-
-    markup = await get_menu_markup([
-        *exercise_markup,
-        navigation_markup
-        ])
-    logger.info(exercise_markup)
-    await message.answer("<b>Выберите упражнение</b>", reply_markup=markup)
-    await state.update_data(exercises=exercises, max_page=max_page)
+    markup = await get_pagination(repo, state, callback_data=ExercisePaginationCallbackData(page_number=1))
+    await message.answer("Выберите упражнение", reply_markup=markup)
 
 
 @exercise_router.callback_query(Role_Filter(user_role=UserRole.USER), ExercisePaginationCallbackData.filter())
-async def check_page_number(call: CallbackQuery, state: FSMContext, callback_data: ExercisePaginationCallbackData) -> None:
-    state_data = await state.get_data()
-    exercises = state_data['exercises']
-    max_page = state_data['max_page']
-
-    current_page = callback_data.page_number
-
-    if current_page == max_page:
-        prev_page = current_page - 1
-        navigation_markup = [
-                 Button(text=f"<< {prev_page}", callback_data=ExercisePaginationCallbackData(page_number=prev_page).pack()),
-                 Button(text=f"{current_page}/{max_page}", callback_data=current_page),
-                ]
-    elif current_page == 1:
-        next_page = current_page + 1
-        navigation_markup = [
-                 Button(text=f"{current_page}/{max_page}", callback_data=current_page),
-                 Button(text=f"{next_page} >>", callback_data=ExercisePaginationCallbackData(page_number=next_page).pack())
-                ]
-    else:
-        prev_page = current_page - 1 
-        next_page = current_page + 1
-        navigation_markup = [
-                 Button(text=f"<< {prev_page}", callback_data=ExercisePaginationCallbackData(page_number=prev_page).pack()),
-                 Button(text=f"{current_page}/{max_page}", callback_data=current_page),
-                 Button(text=f"{next_page} >>", callback_data=ExercisePaginationCallbackData(page_number=next_page).pack())
-                ]
-
-
-    exercise_markup = [[Button(text=exercise, callback_data=exercise)] for exercise in exercises[(current_page-1)*5:current_page*5]]
-    
-    markup = await get_menu_markup([
-        *exercise_markup,
-        navigation_markup
-        ])
-    await call.message.edit_reply_markup(reply_markup=markup)
+async def check_page_number(call: CallbackQuery, state: FSMContext, callback_data: ExercisePaginationCallbackData, repo: UserRepo) -> None:
+    markup = await get_pagination(repo, state, callback_data)
+    await call.message.edit_text("Выберите упражнение", reply_markup=markup)
 
     
 @exercise_router.callback_query(Role_Filter(user_role=UserRole.USER), flags={"database_type": "user_repo"})
